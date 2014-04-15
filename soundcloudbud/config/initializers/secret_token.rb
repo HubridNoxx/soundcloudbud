9b6f85ec3b674ef538403310d870df54c0bf7902
Zack Elliott <hubridnoxx@gmail.com>
2014-04-14 22:16:08 -0400

    first commit

---
+++ soundcloudbud/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soundcloudbud::Application.config.secret_key_base = '0930f1adffccfe416feefa65fdea1a62f08413194863b21598fbe7bdd64521f96481f93ac02055d9566731da227b33419e61d8408510ac1155f0cf141ab92c63'
